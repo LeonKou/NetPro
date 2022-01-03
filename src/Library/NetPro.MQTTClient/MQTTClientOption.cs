7e0959959430a9f109031bc15029dbf87f1f9044
leon <757365800@qq.com>
2022-01-03 23:58:44 +0800

    support mqttclient

---
+++ src/Library/NetPro.MQTTClient/MQTTClientOption.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace NetPro.MQTTClient
+{
+    /// <summary>
+    /// MQTT配置
+    /// </summary>
+    public class MQTTClientOption
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MQTTClientOption()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="config"></param>
+        public MQTTClientOption(IConfiguration config)
+        {
+            if (config == null)
+            {
+                throw new ArgumentNullException(nameof(config));
+            }
+
+            config.GetSection(nameof(MQTTClientOption)).Bind(this);
+
+        }
+
+        /// <summary>
+        /// 连接串集合
+        /// </summary>
+        public List<ConnectionString> ConnectionString { get; set; }
+
+        /// <summary>
+        ///是否启用
+        ///true:启用，false:禁用
+        /// </summary>
+        public bool Enabled { get; set; }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ConnectionString
+    {
+        /// <summary>
+        /// 连接串别名
+        /// </summary>
+        public string Key { get; set; }
+
+        /// <summary>
+        /// 连接串
+        /// 格式：
+        /// clientid=netpro;host=127.0.0.1;port=6379;username=netpro;password=netpro;
+        /// clientid:客户端id唯一标识
+        /// host:broker主机地址
+        /// port:broker主机端口
+        /// username:账户名
+        /// password:密码
+        /// </summary>
+        public string Value { get; set; }
+    }
+}
---
+++ src/Library/NetPro.MQTTClient/ServiceCollectionExtension.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace NetPro.MQTTClient
+{
+    public static class ServiceCollectionExtension
+    {
+        /// <summary>
+        /// MQTT客户端，支持多个host
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
+        public static IServiceCollection AddMQTTClient(this IServiceCollection services, IConfiguration configuration)
+        {
+            var mqtttClientOptions = new MQTTClientOption(configuration);
+            services.AddSingleton(mqtttClientOptions);
+            var idleBus = new IdleBus<IMqttClient>(TimeSpan.FromMinutes(10));
+            foreach (var item in mqtttClientOptions.ConnectionString)
+            {
+                idleBus.Register(item.Key, () =>
+                {
+                    try
+                    {
+                        var mqttClient = new MqttFactory().CreateMqttClient() as IMqttClient; ;
+                        var option = new MqttClientOptionsBuilder()
+                        .WithClientId(_GetItemValueFromConnectionString(item.Value, "clientid"))
+                        .WithCredentials(_GetItemValueFromConnectionString(item.Value, "username"), _GetItemValueFromConnectionString(item.Value, "password"))
+                        .WithTcpServer(_GetItemValueFromConnectionString(item.Value, "host"), int.Parse(_GetItemValueFromConnectionString(item.Value, "port")))
+                        .Build();
+                        mqttClient.ConnectAsync(option);
+                        return mqttClient;
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new ArgumentException($"mqttclient初始化有误{ex}");
+                    }
+                });
+            }
+            services.AddSingleton(idleBus);
+            //https://www.cnblogs.com/ccsharppython/archive/2019/07/28/11261069.html
+            return services;
+
+            string _GetItemValueFromConnectionString(string connectionString, string itemName)
+            {
+                if (!connectionString.EndsWith(";"))
+                    connectionString += ";";
+
+                // \s* 匹配0个或多个空白字符
+                // .*? 匹配0个或多个除 "\n" 之外的任何字符(?指尽可能少重复)
+                string regexStr = itemName + @"\s*=\s*(?<key>.*?);";
+                Regex r = new Regex(regexStr, RegexOptions.IgnoreCase);
+                Match mc = r.Match(connectionString);
+                return mc.Groups["key"].Value;
+            }
+        }
+    }
+}
