6d787030b47ad9eff8258b893487bebd37f9b92f
leon <lyl1002@outlook.com>
2022-01-17 18:21:39 +0800

    netproapi 1.0.3-beta.5

--- src/Template/API/Content/src/XXX.API/Program.cs
+++ src/Template/API/Content/src/XXX.API/Program.cs
@@ -1,4 +1,4 @@
-//using Serilog;
+using Serilog;
 
 Environment.SetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES", "NetPro.Startup");
 
@@ -6,13 +6,13 @@ var host = Host.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration((hostingContext, config) =>
                 {
                     ApolloClientHelper.ApolloConfig(hostingContext, config, args);
-                    //Serilog.Log.Logger = new Serilog.LoggerConfiguration()
-                    // .ReadFrom.Configuration(config.Build())
-                    // .CreateLogger(); //根据需要安装Serilog，并打开注释；相关serilog nuget包已在程序入口所在cspro工程文件中
+                    Serilog.Log.Logger = new Serilog.LoggerConfiguration()
+                     .ReadFrom.Configuration(config.Build())
+                     .CreateLogger(); //根据需要安装Serilog，并打开注释；相关serilog nuget包已在程序入口所在cspro工程文件中
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    //webBuilder.UseSerilog();
+                    webBuilder.UseSerilog();
                     webBuilder.ConfigureKestrel(options =>
                     {
                     });
