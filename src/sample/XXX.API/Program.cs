0b2adc864fba86cc22df5d0a5128c008b265443c
leon <lyl1002@outlook.com>
2022-04-12 00:43:57 +0800

    add  prometheus

--- src/sample/XXX.API/Program.cs
+++ src/sample/XXX.API/Program.cs
@@ -1,7 +1,10 @@
 using Serilog;
 
+Environment.SetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES", "NetPro.Startup");
 var host = Host.CreateDefaultBuilder(args)
-               .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup("NetPro.Startup"))
-               .UseSerilog();//如需serilog日志功能请取消UseSerilog注释
+               .ConfigureWebHostDefaults(webBuilder => { }// webBuilder
+                                                          //.UseSetting(WebHostDefaults.HostingStartupAssembliesKey, "NetPro.Startup;NetPro.Prometheus")
+                                                          //.UseStartup("NetPro.Startup")//以上方式只能取其一，环境变量更灵活
+               ).UseSerilog();//如需serilog日志功能请取消UseSerilog注释；
 
 host.Build().Run();
