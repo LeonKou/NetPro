92d03c4e8c6906a2054297c4a1d66f720b60eacd
leon <lyl1002@outlook.com>
2022-04-16 11:48:03 +0800

    nuget version to 6.0.5-beta.17 for  redis

---
+++ src/Test/Native/Controllers/WeatherForecastController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetPro.CsRedis;
+
+namespace Native.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IRedisManager _redisManager;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,
+            IRedisManager redisManager)
+        {
+            _logger = logger;
+            _redisManager = redisManager;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _redisManager.Get<string>("","1");
+               var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
