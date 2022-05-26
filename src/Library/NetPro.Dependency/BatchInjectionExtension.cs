d2183c87112404da5ca5eaaed73c278a527819d2
leon <lyl1002@outlook.com>
2022-05-26 08:58:12 +0800

    nuget  6.0.8

--- src/Library/NetPro.Dependency/BatchInjectionExtension.cs
+++ src/Library/NetPro.Dependency/BatchInjectionExtension.cs
@@ -25,90 +25,88 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
+using System.NetPro;
 using System.Text.RegularExpressions;
 
-namespace System.NetPro
+/// <summary>
+/// 批量注入
+/// </summary>
+public static class BatchInjectionExtension
 {
     /// <summary>
     /// 批量注入
     /// </summary>
-    public static class BatchInjectionExtension
+    /// <param name="services"></param>
+    /// <param name="assemblyPattern">
+    /// 程序集正则表达式
+    /// 当前程序集名称：this.GetType().Assembly.GetName().Name</param>
+    /// <param name="classNamePattern">批量注入的类名正则表达式</param>
+    /// <remarks>
+    /// 示例：
+    /// Service$：匹配Service结尾的字符串;
+    /// ^XXX.:匹配XXX.开头的字符串;
+    /// *.XXX.*:匹配包含.XXX.的字符串;
+    /// </remarks>
+    public static IServiceCollection BatchInjection(this IServiceCollection services, string assemblyPattern, string classNamePattern = "Service$")
     {
-        /// <summary>
-        /// 批量注入
-        /// </summary>
-        /// <param name="services"></param>
-        /// <param name="assemblyPattern">
-        /// 程序集正则表达式
-        /// 当前程序集名称：this.GetType().Assembly.GetName().Name</param>
-        /// <param name="classNamePattern">批量注入的类名正则表达式</param>
-        /// <remarks>
-        /// 示例：
-        /// Service$：匹配Service结尾的字符串;
-        /// ^XXX.:匹配XXX.开头的字符串;
-        /// *.XXX.*:匹配包含.XXX.的字符串;
-        /// </remarks>
-        public static IServiceCollection BatchInjection(this IServiceCollection services, string assemblyPattern, string classNamePattern = "Service$")
-        {
-            var typeFinder = services.BuildServiceProvider().GetService<ITypeFinder>();
+        var typeFinder = services.BuildServiceProvider().GetService<ITypeFinder>();
 
-            services.Scan(scan => scan
-                .FromAssemblies(typeFinder.GetAssemblies().Where(s => s.GetName().Name.IsValidName(assemblyPattern)))
-                .AddClasses(classes =>
-                classes.Where(type =>
+        services.Scan(scan => scan
+            .FromAssemblies(typeFinder.GetAssemblies().Where(s => s.GetName().Name.IsValidName(assemblyPattern)))
+            .AddClasses(classes =>
+            classes.Where(type =>
+            {
+                if (typeof(IHostedService).IsAssignableFrom(type))
                 {
-                    if (typeof(IHostedService).IsAssignableFrom(type))
-                    {
-                        return false;
-                    }
-                    var succeed = type.Name.IsValidName(classNamePattern) && type.IsClass && !type.IsAbstract;
-                    return succeed;
+                    return false;
                 }
-                ))
-                .AsImplementedInterfaces()
-                .WithScopedLifetime());
-            return services;
-            //template code
-            //services.Scan(scan => scan
-            //  .FromAssemblies(typeFinder.GetAssemblies().Where(s => s.GetName().Name.EndsWith("API")).ToArray())
-            //  .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
-            //  .AsImplementedInterfaces()
-            //  .WithScopedLifetime());
-        }
-
-        /// <summary>
-        /// 接口注入
-        /// </summary>
-        /// <param name="services"></param>
-        public static void InterfaceDependency(this IServiceCollection services)
-        {
-            var typeFinder = services.BuildServiceProvider().GetService<ITypeFinder>();
-
-            services.Scan(scan => scan
-        .FromAssemblyOf<ITransientDependency>()
-        .FromAssemblies(typeFinder.GetAssemblies())
-        .AddClasses(classes => classes.AssignableTo<ITransientDependency>())
-        .AsImplementedInterfaces()
-        .WithTransientLifetime());
-
-            services.Scan(scan => scan
-         .FromAssemblyOf<ISingletonDependency>()
-         .FromAssemblies(typeFinder.GetAssemblies())
-         .AddClasses(classes => classes.AssignableTo<ISingletonDependency>())
-         .AsImplementedInterfaces()
-         .WithSingletonLifetime());
-
-            services.Scan(scan => scan
-            //.FromAssemblyOf<IScopedDependency>()
-            .FromAssemblies(typeFinder.GetAssemblies())
-            .AddClasses(classes => classes.AssignableTo<IScopedDependency>())
+                var succeed = type.Name.IsValidName(classNamePattern) && type.IsClass && !type.IsAbstract;
+                return succeed;
+            }
+            ))
             .AsImplementedInterfaces()
-               .WithScopedLifetime());
-        }
+            .WithScopedLifetime());
+        return services;
+        //template code
+        //services.Scan(scan => scan
+        //  .FromAssemblies(typeFinder.GetAssemblies().Where(s => s.GetName().Name.EndsWith("API")).ToArray())
+        //  .AddClasses(classes => classes.Where(type => type.Name.EndsWith("Service")))
+        //  .AsImplementedInterfaces()
+        //  .WithScopedLifetime());
+    }
 
-        private static bool IsValidName(this string currencyValue, string pattern)
-        {
-            return Regex.IsMatch(currencyValue, pattern);
-        }
+    /// <summary>
+    /// 接口注入
+    /// </summary>
+    /// <param name="services"></param>
+    public static void InterfaceDependency(this IServiceCollection services)
+    {
+        var typeFinder = services.BuildServiceProvider().GetService<ITypeFinder>();
+
+        services.Scan(scan => scan
+    .FromAssemblyOf<ITransientDependency>()
+    .FromAssemblies(typeFinder.GetAssemblies())
+    .AddClasses(classes => classes.AssignableTo<ITransientDependency>())
+    .AsImplementedInterfaces()
+    .WithTransientLifetime());
+
+        services.Scan(scan => scan
+     .FromAssemblyOf<ISingletonDependency>()
+     .FromAssemblies(typeFinder.GetAssemblies())
+     .AddClasses(classes => classes.AssignableTo<ISingletonDependency>())
+     .AsImplementedInterfaces()
+     .WithSingletonLifetime());
+
+        services.Scan(scan => scan
+        //.FromAssemblyOf<IScopedDependency>()
+        .FromAssemblies(typeFinder.GetAssemblies())
+        .AddClasses(classes => classes.AssignableTo<IScopedDependency>())
+        .AsImplementedInterfaces()
+           .WithScopedLifetime());
+    }
+
+    private static bool IsValidName(this string currencyValue, string pattern)
+    {
+        return Regex.IsMatch(currencyValue, pattern);
     }
 }
