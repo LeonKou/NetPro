1d7d451acb16baaaf3f7525d25629cba2b45847b
KamenRiderKuuga <524472212@qq.com>
2022-06-02 16:36:49 +0800

    feat: Adjust the code used to configure the AppMetrics

--- src/Library/NetPro.Prometheus/Startup/HostingPrometheusStartup.cs
+++ src/Library/NetPro.Prometheus/Startup/HostingPrometheusStartup.cs
@@ -1,58 +1,58 @@
-﻿/*
- *  MIT License
- *  
- *  Copyright (c) 2021 LeonKou
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *  
- *  The above copyright notice and this permission notice shall be included in all
- *  copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- *  SOFTWARE.
- */
+﻿///*
+// *  MIT License
+// *  
+// *  Copyright (c) 2021 LeonKou
+// *  
+// *  Permission is hereby granted, free of charge, to any person obtaining a copy
+// *  of this software and associated documentation files (the "Software"), to deal
+// *  in the Software without restriction, including without limitation the rights
+// *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// *  copies of the Software, and to permit persons to whom the Software is
+// *  furnished to do so, subject to the following conditions:
+// *  
+// *  The above copyright notice and this permission notice shall be included in all
+// *  copies or substantial portions of the Software.
+// *  
+// *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// *  SOFTWARE.
+// */
 
-using App.Metrics;
-using App.Metrics.AspNetCore;
-using App.Metrics.Formatters.Prometheus;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using NetPro.Prometheus;
-using System.Linq;
+//using App.Metrics;
+//using App.Metrics.AspNetCore;
+//using App.Metrics.Formatters.Prometheus;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.DependencyInjection;
+//using NetPro.Prometheus;
+//using System.Linq;
 
-[assembly: HostingStartup(typeof(Startup))]
-namespace NetPro.Prometheus
-{
-    internal sealed class Startup : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder)
-        {
-            builder.ConfigureMetrics(AppMetrics.CreateDefaultBuilder()
-                    .OutputMetrics.AsPrometheusPlainText()
-                    .Build()).UseMetrics(options =>
-                    {
-                        options.EndpointOptions = endpointsOptions =>
-                        {
-                            endpointsOptions.MetricsTextEndpointOutputFormatter = Metrics.Instance.OutputMetricsFormatters.OfType<MetricsPrometheusTextOutputFormatter>().First();
-                        };
-                    });
+//[assembly: HostingStartup(typeof(Startup))]
+//namespace NetPro.Prometheus
+//{
+//    internal sealed class Startup : IHostingStartup
+//    {
+//        public void Configure(IWebHostBuilder builder)
+//        {
+//            builder.ConfigureMetrics(AppMetrics.CreateDefaultBuilder()
+//                    .OutputMetrics.AsPrometheusPlainText()
+//                    .Build()).UseMetrics(options =>
+//                    {
+//                        options.EndpointOptions = endpointsOptions =>
+//                        {
+//                            endpointsOptions.MetricsTextEndpointOutputFormatter = Metrics.Instance.OutputMetricsFormatters.OfType<MetricsPrometheusTextOutputFormatter>().First();
+//                        };
+//                    });
 
-            // refer to https://www.app-metrics.io/reporting/collectors
-            builder.ConfigureServices(services =>
-            {
-                services.AddAppMetricsSystemMetricsCollector();
-                services.AddAppMetricsGcEventsMetricsCollector();
-            });
-        }
-    }
-}
+//            // refer to https://www.app-metrics.io/reporting/collectors
+//            builder.ConfigureServices(services =>
+//            {
+//                services.AddAppMetricsSystemMetricsCollector();
+//                services.AddAppMetricsGcEventsMetricsCollector();
+//            });
+//        }
+//    }
+//}
---
+++ src/Library/NetPro.Prometheus/Startup/PrometheusStartup.cs
@@ -0,0 +1,42 @@
+﻿using App.Metrics;
+using App.Metrics.AspNetCore.Endpoints;
+using App.Metrics.Extensions.Configuration;
+using App.Metrics.Formatters.Prometheus;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using System.NetPro;
+
+namespace NetPro.Prometheus
+{
+    internal sealed class PrometheusStartup : INetProStartup, System.NetPro.Startup.__._
+    {
+        public double Order { get; set; } = 0;
+
+        public void Configure(IApplicationBuilder application, IWebHostEnvironment env)
+        {
+            application.UseMetricsAllMiddleware();
+            application.UseMetricsAllEndpoints();
+        }
+
+        public void ConfigureServices(IServiceCollection services, IConfiguration configuration = null, ITypeFinder typeFinder = null)
+        {
+            services.Configure<MetricsEndpointsHostingOptions>(configuration.GetSection(nameof(MetricsEndpointsHostingOptions)));
+
+            var metrics = AppMetrics.CreateDefaultBuilder().Configuration.ReadFrom(configuration).OutputMetrics.AsPrometheusPlainText().Build();
+            services.AddMetrics(metrics)
+                    .AddMetricsTrackingMiddleware(configuration)
+                    .AddMetricsEndpoints(configuration, option =>
+                    {
+                        option.MetricsTextEndpointOutputFormatter = Metrics.Instance.OutputMetricsFormatters.OfType<MetricsPrometheusTextOutputFormatter>().First();
+                    });
+
+            services.AddAppMetricsSystemMetricsCollector();
+            services.AddAppMetricsGcEventsMetricsCollector();
+        }
+    }
+
+}
